71c493ea2dc9cebfc96d93dbee8b444cd8196392
Konya Timea <konya.timea2014@gmail.com>
2026-07-26 16:32:34 +0300

    asteroid game ending and unlocked characters

--- reward/symbol_reveal.gd
+++ reward/symbol_reveal.gd
@@ -12,11 +12,17 @@ signal symbols_ready(letters: Array)
 
 func _ready() -> void:
 	randomize()
-	chosen_letters = _pick_random_letters(3)
+	var translator := get_node_or_null("/root/TranslationManager")
+	chosen_letters = _pick_random_letters(3, translator)
 
-	for i in symbol_labels.size():
-		var letter: String = chosen_letters[i]
-		symbol_labels[i].text = _display_text_for(letter)
+	for i in chosen_letters.size():
+		symbol_labels[i].text = _display_text_for(chosen_letters[i])
+	for i in range(chosen_letters.size(), symbol_labels.size()):
+		symbol_labels[i].visible = false
+
+	if translator:
+		for letter in chosen_letters:
+			translator.unlock_letter(letter)
 
 	symbols_ready.emit(chosen_letters)
 
@@ -25,21 +31,22 @@ func _ready() -> void:
 	tween.tween_property(fade, "modulate:a", 0.0, 0.6) \
 		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
-
-func _pick_random_letters(count: int) -> Array[String]:
+func _pick_random_letters(count: int, translator) -> Array[String]:
 	var pool: Array[String] = []
 	for letter in ALPHABET:
-		pool.append(letter)
+		if translator == null or not translator.unlocked_letters.get(letter, false):
+			pool.append(letter)
 	pool.shuffle()
+	var take: int = min(count, pool.size())
 	var picked: Array[String] = []
-	for i in count:
+	for i in take:
 		picked.append(pool[i])
 	return picked
 
 func _display_text_for(letter: String) -> String:
 	var translator := get_node_or_null("/root/TranslationManager")
 	var symbol_text: String = letter.to_upper() if translator == null else translator.get_translated_text(letter)
-	return "%s [font_size=40][color=#8fa3b8]=[/color] %s[/font_size]" % [symbol_text, letter.to_upper()]
+	return "%s [font_size=32][color=#8fa3b8]=[/color] %s[/font_size]" % [symbol_text, letter.to_upper()]
 
 func unlock_and_refresh(letter: String) -> void:
 	var translator := get_node_or_null("/root/TranslationManager")
